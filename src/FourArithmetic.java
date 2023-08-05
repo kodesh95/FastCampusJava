12ca12cf66e8e0eaec6150c32e0ae8f9357a97ae
kodesh95 <kodesh95@gamil.com>
2023-08-05 21:40:25 +0900

    java study

---
+++ src/FourArithmetic.java
@@ -0,0 +1,14 @@
+public class FourArithmetic {
+    public static void main(String[] args) {
+        int n1,n2;
+
+        n1=12;
+        n2=2;
+
+        System.out.printf("%d + %d = %d%n",n1,n2,n1+n2);
+        System.out.printf("%d - %d = %d%n",n1,n2,n1-n2);
+        System.out.printf("%d * %d = %d%n",n1,n2,n1*n2);
+        System.out.printf("%d / %d = %.1f%n",n1,n2,n1/(float)n2);
+
+    }
+}
