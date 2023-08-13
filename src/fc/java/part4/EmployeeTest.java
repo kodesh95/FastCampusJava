c42cab9adef7fdac3785e74d66f6d1bd15c72ac5
kodesh95 <kodesh95@gamil.com>
2023-08-13 09:18:39 +0900

    java study

---
+++ src/fc/java/part4/EmployeeTest.java
@@ -0,0 +1,29 @@
+package fc.java.part4;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+//        일반사원 한 명의 객체 생성 후 데이터 저장 후 출력
+//        RempVO vo = new RempVO();
+
+//        자식이 부모의 기억공간에 초기화를 하는 경우
+//        vo.name="홍길동";
+//        vo.age=22;
+//        vo.phone="010-1111-1111";
+//        vo.empDate="2023-10-11";
+//        vo.dept="marketing";
+//        vo.marriage=true;
+//
+//        System.out.println("vo.name = " + vo.name);
+//        System.out.println("vo.name = " + vo.age);
+//        System.out.println("vo.name = " + vo.phone);
+//        System.out.println("vo.name = " + vo.empDate);
+//        System.out.println("vo.name = " + vo.dept);
+//        System.out.println("vo.name = " + vo.marriage);
+//        System.out.println(vo);
+
+//        부모에게 초기화를 부탁하는 경우
+
+        RempVO vo = new RempVO("홍길동",55,"010-1111-1111","2022-02-10","marketing",true);
+        System.out.println(vo);
+    }
+}
---
+++ src/fc/java/part4/RempVO.java
@@ -0,0 +1,33 @@
+package fc.java.part4;
+
+import fc.java.part4.Employee;
+
+/**
+ * RempVO
+ * : 사원 VO
+ * @author comet
+ * @version 17
+ * date of preparation 2023-08-12
+ * time 오전 8:27
+**/
+public class RempVO extends Employee {
+//    부모의 생성자에서 초기화하는 것이 바람직하다.
+    public RempVO() {
+
+        super();
+    }
+
+    public RempVO(String name,int age,String phone,String empDate,String dept,boolean marriage) {
+//        자식이 부모의 기억공간에 초기화를 하는 경우
+//        this.name=name;
+//        this.age=age;
+//        this.phone=phone;
+//        this.empDate=empDate;
+//        this.dept=dept;
+//        this.marriage=marriage;
+
+//        부모에게 초기화를 부탁하는 경우
+        super(name, age, phone, empDate, dept, marriage);
+
+    }
+}
