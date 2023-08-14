3866b74cbeb1ce58a143c831f100ee81f93ba967
kodesh95 <kodesh95@gamil.com>
2023-08-15 07:28:40 +0900

    java study

---
+++ src/fc/java/model/Cat.java
@@ -0,0 +1,14 @@
+package fc.java.model;
+
+public class Cat extends Animal {
+    public Cat() {
+        super();
+    }
+    public void eat() {
+        System.out.println("고양이처럼 먹다.");
+    }
+    public void flex(){
+
+        System.out.println("고양이는 유연하다.");
+    }
+}
